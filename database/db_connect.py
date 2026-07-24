bd5cb25d2165721fb18092fddd7aca469d5fdb35
Hasnain Khan <hasnainkh3011@gmail.com>
2026-07-24 13:03:18 +0530

    Add database connection

---
+++ database/db_connect.py
@@ -0,0 +1,16 @@
+import sqlite3
+import os
+
+DB_PATH = os.path.join(os.dirname(__file__),'clv.db')
+
+def get_connection():
+    conn = sqlite3.connect(DB_PATH)
+    return conn
+
+def create_tables():
+    conn = get_connection()
+    with open('database/schema.sql','r') as f:
+        schema = f.read()
+    conn.executescript(schema)
+    conn.commit()
+    conn.close
