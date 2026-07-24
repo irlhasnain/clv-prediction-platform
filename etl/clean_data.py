9aff86de1d6190700df4514739caec0860765b56
Hasnain Khan <hasnainkh3011@gmail.com>
2026-07-24 23:46:07 +0530

    Add clean data script

--- etl/clean_data.py
+++ etl/clean_data.py
@@ -2,3 +2,20 @@ import pandas as pd
 
 def clean_data(input_path,output_path):
     df = pd.read_csv(input_path, encoding='latin-1')
+
+    df = df.dropna()
+    df = df.drop_duplicates()
+
+    df.columns = [c.strip().lower().replace(' ','_').replace('-','_') for c in df.columns]
+
+    df['order_date'] = pd.to_datetime(df['order_date'], errors = 'coerce')
+    df['ship_date'] = pd.to_datetime(df['ship_date'], errors = 'coerce')
+
+    df = df.dropna(subset=['customer_name', 'product_name'])
+
+    df.to_csv(output_path, index= False)
+    print(f"Cleaned data saved: {df.shape}")
+    return df 
+
+if __name__ == "__main__":
+    clean_data("data/raw/Sample - Superstore.csv", "data/processed/cleaned_data.csv")
